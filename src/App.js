3b4536de0d9d93b448c48c42908b1d98baf6d5a9
25 macamp <rateliasudaisuki@gmail.com>
2019-08-18 14:54:26 +0900

    Create ProjectDetails Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Layout/Navbar";
 import Dashboard from "./components/Dashboard/Dashboard";
+import ProjectDetails from "./components/Projects/ProjectDetails";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         <Navbar />
       </div>
       <Switch>
-        <Route path="/" component={Dashboard} />>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/project/:id" component={ProjectDetails} />
       </Switch>
     </BrowserRouter>
   );
