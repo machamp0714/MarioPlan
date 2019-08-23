da823fa52d96e888b8dd42ef44ffd4d7af38d604
25 macamp <rateliasudaisuki@gmail.com>
2019-08-23 17:33:43 +0900

    Logging Users In

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn } from "../../Store/actions/authActions";
 
 export class SignIn extends Component {
   state = {
@@ -14,10 +16,11 @@ export class SignIn extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signIn(this.state);
   };
 
   render() {
+    const { authError } = this.props;
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -32,6 +35,9 @@ export class SignIn extends Component {
           </div>
           <div className="input-field">
             <button className="btn pink light-1 z-depth-0">Login</button>
+            <div className="red-text center">
+              {authError ? <p>{authError}</p> : null}
+            </div>
           </div>
         </form>
       </div>
@@ -39,4 +45,21 @@ export class SignIn extends Component {
   }
 }
 
-export default SignIn;
+const mapDispatchProps = (dispatch) => {
+  return {
+    signIn: (credentials) => {
+      dispatch(signIn(credentials));
+    }
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchProps
+)(SignIn);
