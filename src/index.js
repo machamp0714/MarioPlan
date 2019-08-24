83a53972fbef9be5b2665dfdad5057d1d436de02
25 macamp <rateliasudaisuki@gmail.com>
2019-08-24 11:15:19 +0900

    Waiting for Auth Ready

--- src/index.js
+++ src/index.js
@@ -16,18 +16,18 @@ const store = createStore(
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
     reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    reactReduxFirebase(fbConfig, { attachAuthIsReady: true })
   )
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// authが帰ってくるのを待っている。
+// これがないとログインしているにも関わらず、ログインしていない状態のViewが描画されてから、ログイン状態のViewがレンダリングされてしまう。
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+  serviceWorker.unregister();
+});
