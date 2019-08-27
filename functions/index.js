cffa761a0c34990faf9e7a7b1de191b8a2145c33
25 macamp <rateliasudaisuki@gmail.com>
2019-08-27 11:10:27 +0900

    Cloud Function Firestore Trigger_s

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello Tatsube!");
 });
+
+const createNotification = (notification) => {
+  return admin
+    .firestore()
+    .collection("notification")
+    .add(notification)
+    .then((doc) => {
+      console.log("Added Notification", doc);
+    });
+};
+
+exports.createdProgect = functions.firestore
+  .document("projects/{projectId}")
+  .onCreate((doc) => {
+    const project = doc.data();
+    const notification = {
+      content: "Add a new project",
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    return createNotification(notification);
+  });
